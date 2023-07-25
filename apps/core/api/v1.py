f20c805a5d7d44add1ca60ed33530248c712652d
kevortega <kevortega.dev@gmail.com>
2023-07-25 20:59:01 -0300

    fix(user, v1): user id customized deleted, get_v1 function changed to get_livez

--- apps/core/api/v1.py
+++ apps/core/api/v1.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
-def get_v1(request):
+
+def get_livez(request):
     return HttpResponse(True)
--- constructor/urls.py
+++ constructor/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from apps.core.api.v1 import get_v1
+from apps.core.api.v1 import get_livez
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('livez/', get_v1),
+    path('livez/', get_livez),
 ]
